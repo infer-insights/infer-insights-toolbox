d12903e86d187486f2c6e35e60c6c3ec8963d150
infer-insights <bitspotdotpl@gmail.com>
2026-09-11 22:18:07 +0200

    toolbox documentation init

---
+++ practise-queries.sql
@@ -0,0 +1,91 @@
+
+/*
+============================================================
+SQL Toolbox - Examples of applied solutions
+Catalog: T-SQL
+============================================================
+*/
+
+-- #1. vending machines utilization limited to machines with turnover in selected city
+
+use BCOverview_Vending;
+
+WITH VOL_COUNT_DIS AS
+(
+SELECT
+	VEND_ID,
+	COUNT(DISTINCT LP) AS VOLUME_DIS
+FROM
+	UTILIZATION
+GROUP BY
+	VEND_ID
+)
+SELECT
+	p.VEND_ID,
+	COUNT (DISTINCT p.VEND_ID) AS VEND_AUTO_ACTIVE_COUNT,
+	v.VOLUME_DIS
+
+FROM	
+	VEND_TABLE p
+
+INNER JOIN
+	VOL_COUNT_DIS v ON p.VEND_ID = v.VEND_ID
+	
+WHERE
+	p.NETWORK_TYPE = 'DUAL_PAYMENT' AND 
+	v.VOLUME_DIS > 0 AND
+	p.CITY = 'Example_City'
+GROUP BY
+	p.VEND_ID,
+	v.VOLUME_DIS
+ORDER BY
+	v.VOLUME_DIS DESC;
+
+
+    use BCOverview_VENDING;
+
+
+-- #2. most popular vending machines locations utilization limited to selected year, network, city and service type
+
+WITH VOL_COUNT_DIS AS
+(
+SELECT
+	VEND_ID,
+	COUNT(DISTINCT LP) AS VOLUME_DIS,
+	LEFT(CONVERT(VARCHAR(10), DATA_SP, 23), 4) AS YEAR
+FROM
+	UTILIZATION
+WHERE
+	STATUS = 'FILLED' AND								
+	LEFT(CONVERT(VARCHAR(10), DATA_SP, 23), 4) = '2025'
+GROUP BY
+	VEND_ID, LEFT(CONVERT(VARCHAR(10), DATA_SP, 23), 4)
+),
+
+--rank calculation in second cte
+
+RANKED_VEND AS
+(
+SELECT
+	v.VEND_ID,
+	v.VOLUME_DIS,
+	CONCAT (p.CITY,', ',p.STREET) AS LOCATION_DET,
+	DENSE_RANK() OVER (ORDER BY v.VOLUME_DIS DESC) AS VEND_RANK
+FROM	
+	VEND_DB p
+INNER JOIN
+	VOL_COUNT_DIS v ON p.VEND_ID = v.VEND_ID
+WHERE
+	p.NETWORK_TYPE = 'SINGLE_PAYMENT'
+)
+--summary table display top 10 selection
+
+SELECT 
+	p.VEND_RANK AS RANK,
+	p.VEND_ID AS ID,
+	p.LOCATION_DET AS LOCATION,
+	p.VOLUME_DIS AS 'FILLED VOL'
+FROM
+	RANKED_VEND p
+WHERE
+	VEND_RANK <= 10;
---
+++ snowflake-memo.sql
@@ -0,0 +1,20 @@
+/*
+==================================================
+PROJECT: SNOWFLAKE HANDS ON OPEN PRACTISE
+Date: Sep 2026
+
+Environment Practise Best Practise and code examples:
+1. Warehouse Cost Optimization
+2. 
+==================================================
+*/
+
+--Decrease warehouse standby acticity and auto scalling for sql code cost optimisation 
+
+ALTER ACCOUNT SET DATA_RETENTION_TIME_IN_DAYS = 1;
+-- KROK B: Wh acticity limit to 60s
+ALTER WAREHOUSE COMPUTE_WH SET AUTO_SUSPEND = 60 AUTO_RESUME = TRUE;
+-- Query acceleration limit to 2
+ALTER WAREHOUSE COMPUTE_WH SET 
+    ENABLE_QUERY_ACCELERATION = TRUE 
+    QUERY_ACCELERATION_MAX_SCALE_FACTOR = 2;
